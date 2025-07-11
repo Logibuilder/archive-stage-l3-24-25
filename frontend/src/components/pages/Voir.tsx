79186a008ec109c971d84968a2ffc461ffc651e7
Logibuilder <161773002+Logibuilder@users.noreply.github.com>
2025-07-11 18:57:51 +0500

    Delete frontend directory

--- frontend/src/components/pages/Voir.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-import { print_doc } from "../../utils";
-
-export function Voir() {
-    const location = useLocation();
-    const doc = location.state!.doc;
-
-    return print_doc(doc);
-}
